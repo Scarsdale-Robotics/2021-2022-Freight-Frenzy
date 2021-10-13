376aee9cbc80b0b8c2999315098ba9287526dcbd
ecukier <ephramcukier@icloud.com>
2021-10-13 16:31:01 -0400

    basic teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpControl.java
@@ -5,11 +5,20 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "TeleOp")
 public class TeleOpControl extends OpMode {
+
+    HardwareRobot robot = new HardwareRobot(hardwareMap);
+    MovementController mController = new MovementController(robot, telemetry);
     @Override
     public void init() {
     }
 
     @Override
     public void loop() {
+        if(gamepad1.left_stick_y > 0.5){
+            mController.moveForward(0.2);
+        }
+        if(gamepad1.left_stick_y < -0.5){
+            mController.moveBackwards(0.2);
+        }
     }
 }
