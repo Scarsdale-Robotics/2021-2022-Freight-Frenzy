439743fa38621d6d8d1a6496f6bf4a7825087bed
Andihedral <adamymz@gmail.com>
2021-10-28 12:18:17 -0400

    this is wrong code but im saving it for safety

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/posDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/posDemo.java
@@ -1,15 +1,48 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 @Autonomous(name = "posDemo_auto")
-public class posDemo {
+public class posDemo extends LinearOpMode {
 
     public void getToPos(double outerL, double innerL, double heading) {
+        HardwareRobot robot = new HardwareRobot(hardwareMap);
+        MovementController mController = new MovementController(robot, telemetry);
+        while(robot.imu.getAngularOrientation() < -90) {
+            mController.rotate(1);
+            mController.update();
+        }
+        mController.stop();
+        mController.update();
+        while(robot.backDist.getDistance(DistanceUnit.INCH) < innerL) {
+            mController.drive(0.2);
+            mController.update();
+        }
+        while(robot.imu.getAngularOrientation() != 180) {
+            mController.rotate(1);
+            mController.update();
+        }
+        while(robot.backDist.getDistance(DistanceUnit.INCH) < innerL) {
+            mController.drive(0.2);
+            mController.update();
+        }
+        while(robot.imu.getAngularOrientation() != heading) {
+            mController.rotate(1);
+            mController.update();
+        }
+
+
+
 
     }
 
 
+    @Override
+    public void runOpMode() throws InterruptedException {
 
+    }
 }
