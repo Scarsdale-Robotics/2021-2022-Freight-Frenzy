6d33403aa22022455388748bd4a8db34ac89fd41
ecukier <ephramcukier@icloud.com>
2022-01-18 10:24:25 -0500

    places block on level warehouse autored

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WarehouseAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WarehouseAutoRed.java
@@ -28,10 +28,45 @@ public class WarehouseAutoRed extends LinearOpMode {
 
         waitForStart();
         int duckPos = duckDetector.getDuckPosition();
+        telemetry.addData("reeee: ", duckPos);
+        telemetry.update();
 
 
+        //move back
+        mController.joystickMovement(0, -0.5);
+        mController.update();
+        while(opModeIsActive() && (robot.frontDist.getDistance(DistanceUnit.INCH) < 5 || robot.frontDist.getDistance(DistanceUnit.CM) > 999));
+        mController.stop();
+        mController.update();
+
+        robot.clawArm.setTargetPosition(1600);
 
 
+        while(opModeIsActive() && robot.clawArm.isBusy());
+
+        float startAngle = robot.imu.getAngularOrientation().firstAngle;
+        mController.rotate(-0.8);
+        mController.update();
+        while(opModeIsActive() && startAngle - robot.imu.getAngularOrientation().firstAngle > -15);
+        mController.stop();
+        mController.update();
+
+
+        int levels[] = {3491, 3965, 4408};
+        if (duckPos == -1){
+            duckPos = 0;
+        }
+        robot.clawArm.setTargetPosition(levels[duckPos]);
+        while(opModeIsActive() && robot.clawArm.isBusy());
+
+        mController.joystickMovement(0, -0.7);
+        mController.update();
+        startTimer = System.currentTimeMillis();
+        while (opModeIsActive() && System.currentTimeMillis() - startTimer < 1500);
+        mController.stop();
+        mController.update();
+
+        mController.openClaw();
         while(opModeIsActive());
 
 
