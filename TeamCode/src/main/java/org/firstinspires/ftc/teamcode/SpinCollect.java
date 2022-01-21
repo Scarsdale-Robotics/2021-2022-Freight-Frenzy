42aa467a2103c9729270c6b6402a1593321940c5
mzhang23 <max2004zhang@gmail.com>
2022-01-21 14:43:56 -0500

    spin collect -max

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinCollect.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinCollect.java
@@ -12,60 +12,40 @@ public class SpinCollect extends LinearOpMode {
         HardwareRobot robot = new HardwareRobot(hardwareMap);
         MovementController mController = new MovementController(robot, telemetry);
 
-        //Starting position: facing straight towards the pile, claw open and in position
-        mController.setPosition(0.5);
+        //Starting position: facing straight towards the pile, claw open and down
+        //Initialize & start
+        mController.lift(0);
+        mController.openClaw();
+
+        // get close to wall
         mController.drive(1);
         mController.update();
+        while (opModeIsActive() && robot.frontDist.getDistance(DistanceUnit.INCH) > 18);
 
-        while (opModeIsActive() && robot.frontDist.getDistance(DistanceUnit.INCH) > 10) {}
-
-        //position: facing wall
-        //spin 180 degrees
-        //firstAngle???
+        //position: facing wall, 10 inches away
+        //spin 90 degrees left while closing claw
         double startingDegrees = robot.imu.getAngularOrientation().firstAngle;
-        while (opModeIsActive() && robot.imu.getAngularOrientation().firstAngle-startingDegrees < 180) {
-            mController.rotationalForward(1, 0.5);
-            mController.update();
-        }
+        mController.closeCLaw();
+        mController.rotate(1);
+        mController.update();
+        while (opModeIsActive() && robot.imu.getAngularOrientation().firstAngle-startingDegrees > -90);
+
+        //
+        // pin back into a slightly angled position while lifting claw
+        mController.lift(2);
+        mController.rotate(-1);
+        mController.update();
+        while (opModeIsActive() && robot.imu.getAngularOrientation().firstAngle-startingDegrees < 25);
+
+        //drive to the tower
+        mController.drive(-1);
+        mController.update();
+        while(opModeIsActive() && robot.frontDist.getDistance(DistanceUnit.INCH)<33.5);
+
+        //drop off the cargo
         mController.stop();
+        mController.openClaw();
         mController.update();
-
-    }
-}
-
-
-package org.firstinspires.ftc.teamcode;
-
-        import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-        import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-@Autonomous(name="SpinCollect")
-public class SpinCollect extends LinearOpMode {
-    @Override
-    public void runOpMode() {
-        HardwareRobot robot = new HardwareRobot(hardwareMap);
-        MovementController mController = new MovementController(robot, telemetry);
-
-        //Starting position: facing straight towards the pile, claw open and in position
-        mController.setPosition(0.5);
-        mController.drive(1);
-        mController.update();
-
-        while (opModeIsActive() && robot.frontDist.getDistance(DistanceUnit.INCH) > 10) {}
-
-        //position: facing wall
-        //spin 180 degrees
-        //firstAngle???
-        double startingDegrees = robot.imu.getAngularOrientation().firstAngle;
-        while (opModeIsActive() && robot.imu.getAngularOrientation().firstAngle-startingDegrees < 180) {
-            mController.rotationalForward(1, 0.5);
-            mController.update();
-        }
-        mController.stop();
-        mController.update();
-
     }
 }
 
