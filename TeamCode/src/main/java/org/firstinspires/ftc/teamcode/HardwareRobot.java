45a82c6700d4ccc8d4f3fb76b79b131dd77f56e1
Yey007 <yey007tech@gmail.com>
2022-01-21 15:13:09 -0500

    hardware robot cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
@@ -14,15 +14,9 @@ public class HardwareRobot {
     public DcMotor leftBack = null;
     public DcMotor rightFront = null;
     public DcMotor rightBack = null;
+
     public DcMotor duckSpinLeft = null;
     public DcMotor duckSpinRight = null;
-
-
-    public DcMotor elevatorCable = null;
-    public DcMotor elevatorIntake = null;
-
-
-    public Servo elevatorDoor = null;
 
     public Rev2mDistanceSensor frontDist = null;
     public Rev2mDistanceSensor leftDist = null;
@@ -31,12 +25,9 @@ public class HardwareRobot {
 
     public BNO055IMU imu = null;
 
-
     public DcMotor clawArm = null;
     public Servo clawLeft = null;
     public Servo clawRight = null;
-
-
 
     HardwareMap hwMap;
 
@@ -47,8 +38,6 @@ public class HardwareRobot {
 
     private void init()
     {
-
-
         imu = hwMap.get(BNO055IMU.class, "imu");
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -61,7 +50,6 @@ public class HardwareRobot {
 
         imu.initialize(parameters);
 
-
         // match motors to phone labels
         leftFront = hwMap.dcMotor.get("leftFront");
         leftBack = hwMap.dcMotor.get("leftBack");
@@ -71,31 +59,18 @@ public class HardwareRobot {
         duckSpinRight = hwMap.dcMotor.get("duckSpinRight");
         duckSpinLeft = hwMap.dcMotor.get("duckSpinLeft");
 
-
-//        elevatorCable = hwMap.dcMotor.get("elevatorCable");
-//        elevatorIntake = hwMap.dcMotor.get("elevatorIntake");
-//
-//        elevatorDoor = hwMap.servo.get("elevatorDoor");
-
-
         clawArm = hwMap.dcMotor.get("clawArm");
 
         clawLeft = hwMap.servo.get("clawLeft");
         clawRight = hwMap.servo.get("clawRight");
-
-
-//        elevatorDoor.scaleRange(0, 1);
         clawLeft.scaleRange(0, 1);
         clawRight.scaleRange(0, 1);
-
-
 
         // sensors
         frontDist = hwMap.get(Rev2mDistanceSensor.class, "frontDist");
         backDist = hwMap.get(Rev2mDistanceSensor.class, "backDist");
         leftDist = hwMap.get(Rev2mDistanceSensor.class, "leftDist");
         rightDist = hwMap.get(Rev2mDistanceSensor.class, "rightDist");
-
 
         // set direction of motors
         leftFront.setDirection(DcMotor.Direction.REVERSE);
@@ -105,11 +80,7 @@ public class HardwareRobot {
         duckSpinLeft.setDirection(DcMotor.Direction.REVERSE);
         duckSpinRight.setDirection(DcMotor.Direction.REVERSE);
 
-//        elevatorIntake.setDirection(DcMotor.Direction.REVERSE);
-//        elevatorCable.setDirection(DcMotor.Direction.REVERSE);
         clawArm.setDirection(DcMotor.Direction.REVERSE);
-
-
 
         // set power for dc motors and position of servos
         leftFront.setPower(0);
@@ -118,49 +89,26 @@ public class HardwareRobot {
         rightBack.setPower(0);
         duckSpinLeft.setPower(0);
         duckSpinRight.setPower(0);
-
-//        elevatorCable.setPower(0);
-//        elevatorIntake.setPower(0);
         clawArm.setPower(0);
 
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-
         duckSpinLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         duckSpinRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-//        elevatorIntake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        elevatorCable.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         clawArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
-        // set motor modes
-//        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         duckSpinLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         duckSpinRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-//        elevatorIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        elevatorCable.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        clawArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
-
-        clawArm.setTargetPosition(0);
-//        elevatorCable.setTargetPosition(0);
 
         leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        clawArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        clawArm.setTargetPosition(0);
         clawArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        elevatorCable.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
     }
 }
