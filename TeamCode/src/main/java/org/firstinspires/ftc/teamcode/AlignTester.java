5d80f0d70458cbbe8349134845438ab50b5c09a7
Yey007 <yey007tech@gmail.com>
2022-02-10 15:19:59 -0500

    tune values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlignTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlignTester.java
@@ -1,12 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.vision.AutoAlignCV;
 
@@ -14,26 +9,26 @@ import org.firstinspires.ftc.teamcode.vision.AutoAlignCV;
 public class AlignTester extends LinearOpMode {
     @Override
     public void runOpMode() {
-        HardwareRobot robot = new HardwareRobot(hardwareMap);
-        MovementController mController = new MovementController(robot, this);
-
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         AutoAlignCV detector = new AutoAlignCV(cameraMonitorViewId);
 
         waitForStart();
 
+        HardwareRobot robot = new HardwareRobot(hardwareMap);
+        MovementController mController = new MovementController(robot, this);
+
         while (opModeIsActive()) {
             int x = detector.getXPosition();
             int width = detector.getItemWidth();
             telemetry.addData("Position: ", x);
-            telemetry.addData("Area: ", width);
+            telemetry.addData("Width: ", width);
 
             if (x < 140) {
                 mController.pivotOnLeft(-0.7);
             } else if (x > 180) {
                 mController.pivotOnRight(-0.7);
             } else {
-                if(width > 60) {
+                if(width > 30) {
                     mController.drive(-0.7);
                 } else {
                     mController.stop();
